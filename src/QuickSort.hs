639608bd1105ff21338134736fed207cc7b90e1d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-07-09 17:33:54 +0200

    Add quickCheck test idempotency for quickSort.

--- src/QuickSort.hs
+++ src/QuickSort.hs
@@ -1,6 +1,14 @@
-module QuickSort where
+{-# LANGUAGE TemplateHaskell #-}
+
+module QuickSort (
+                 quickSort1,
+                 quickSort2
+                 ) where
 
 import Data.List (partition)
+import Test.QuickCheck
+import Test.QuickCheck.All
+
 
 quickSort1 [] = []
 quickSort1 (x:xs) = quickSort1 [y | y <- xs, y < x] ++ [x] ++ quickSort1 [y | y <- xs, y > x]
@@ -8,3 +16,12 @@ quickSort1 (x:xs) = quickSort1 [y | y <- xs, y < x] ++ [x] ++ quickSort1 [y | y 
 quickSort2 [] = []
 quickSort2 (x:xs) = quickSort2 ys ++ [x] ++ quickSort2 zs
     where (ys, zs) = partition (< x) xs
+
+
+prop_quicksort_idemp :: [Int] -> Bool
+prop_quicksort_idemp xs = quickSort1 (quickSort1 xs) == quickSort1 xs
+
+return []
+main = $quickCheckAll
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE TemplateHaskell #-}
+
+import Test.QuickCheck
+import Test.QuickCheck.All
+import QuickSort (quickSort1, quickSort2)
+
+
+prop_quicksort_idemp :: [Int] -> Bool
+prop_quicksort_idemp xs = quickSort1 (quickSort1 xs) == quickSort1 xs
+
+
+return []
+main = $quickCheckAll
+
