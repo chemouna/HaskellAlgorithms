abe2ce0e43f7949a7799d16445c0a36051c33732
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-07-09 17:43:13 +0200

    Add some quickCheck properties tests.

--- src/QuickSort.hs
+++ src/QuickSort.hs
@@ -5,7 +5,7 @@ module QuickSort (
                  quickSort2
                  ) where
 
-import Data.List (partition)
+import Data.List
 import Test.QuickCheck
 import Test.QuickCheck.All
 
@@ -17,9 +17,15 @@ quickSort2 [] = []
 quickSort2 (x:xs) = quickSort2 ys ++ [x] ++ quickSort2 zs
     where (ys, zs) = partition (< x) xs
 
-
 prop_quicksort_idemp :: [Int] -> Bool
 prop_quicksort_idemp xs = quickSort1 (quickSort1 xs) == quickSort1 xs
+
+prop_quicksort_nn_min xs = not (null xs) ==> head (quickSort1 xs) == minimum xs
+
+prop_quicksort_nn_max xs = not (null xs) ==> head (reverse (quickSort1 xs)) == maximum xs
+
+prop_quicksort_sort xs = quickSort1 xs == sort xs
+
 
 return []
 main = $quickCheckAll
