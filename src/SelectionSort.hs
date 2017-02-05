08588cf47f246391d61ce8c7bb7cbd4d1f4035e4
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-02-05 20:24:58 +0100

    Selection sort.

---
+++ src/SelectionSort.hs
@@ -0,0 +1,10 @@
+module SelectionSort where
+
+import Data.List
+
+selectionSort :: Ord a => [a] -> [a]
+selectionSort [] = []
+selectionSort [x] = [x]
+selectionSort xs = selectionSort (delete x xs) ++ [x]
+  where x = maximum xs
+
