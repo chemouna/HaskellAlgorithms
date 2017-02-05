766ab912d054325190119f593080333cd3045e92
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-02-05 19:18:00 +0100

    Merge sort solution #2.

--- src/MergeSort.hs
+++ src/MergeSort.hs
@@ -1,4 +1,3 @@
-
 module MergeSort where
 
 mergeSort :: Ord a => [a] -> [a]
@@ -21,3 +20,12 @@ split (x:y:zs) = let (xs, ys) = split zs in (x:xs, y:ys)
 split [x] = ([x], [])
 split [] = ([], [])
 
+mergeSort2 :: Ord a => [a] -> [a]
+mergeSort2 xs
+  | length xs > 1 = merge (mergeSort2 ls) (mergeSort2 rs)
+  | otherwise = xs
+  where (ls, rs) = splitInHalf xs
+
+splitInHalf :: Ord a => [a] -> ([a], [a])
+splitInHalf xs = (take n xs, drop n xs)
+  where n = length xs `div` 2
