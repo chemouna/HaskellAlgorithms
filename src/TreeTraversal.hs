c6a8b7d34c4774cf0a6ef488ff5db59108c34f41
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-02-25 16:02:29 +0100

    Suite tree traversal

--- src/TreeTraversal.hs
+++ src/TreeTraversal.hs
@@ -5,7 +5,7 @@ data Tree a = Empty
   | Node a (Tree a) (Tree a)
   deriving (Show, Eq)
 
-preorder, inorder, postorder :: Tree a -> [a]
+preorder, inorder, postorder, levelorder :: Tree a -> [a]
 preorder Empty = []
 preorder (Node v l r) = v : preorder l ++ preorder r
 
@@ -15,3 +15,6 @@ inorder (Node v l r) = inorder l ++ (v : inorder r)
 postorder Empty = []
 postorder (Node v l r) = postorder l ++ postorder r ++ [v]
 
+levelorder Empty = []
+levelorder (Empty:xs) = levelorder xs
+levelorder (Node v l r:xs) = v:levelorder (xs ++ [l, r]) 
