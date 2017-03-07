8afe3f60258a8c6150af85466b916fcf17ee103e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-07 13:14:22 +0100

    Selection test

--- src/MedianOfMediansSelect.hs
+++ src/MedianOfMediansSelect.hs
@@ -7,12 +7,11 @@ import Control.Arrow ((&&&))
 indexOfMedian :: Int -> Int
 indexOfMedian n = (n - 1) `div` 2
 
-bruteForceMedian :: Ord a => [a] -> a
-bruteForceMedian xs = sort xs !! indexOfMedian (length xs)
+bfMedian :: Ord a => [a] -> a
+bfMedian xs = sort xs !! indexOfMedian (length xs)
 
 pivot :: Ord a => a -> [a] -> ([a], [a])
 pivot x = filter (< x) &&& filter (> x)
-
 
 select :: (Ord a, Show a) => Int -> [a] -> a
 select _ [] = error "Empty List"
@@ -20,7 +19,10 @@ select 0 [x] = x
 select k xs | k < s = select k smaller
             | k == s = x
             | otherwise = select (k - s - 1) larger
-            where medians = map bruteForceMedian $ chunksOf 5 xs
+            where medians = map bfMedian $ chunksOf 5 xs
                   x = select (indexOfMedian $ length medians) medians 
                   (smaller, larger) = pivot x xs
                   s = length smaller
+
+testSelect :: Bool
+testSelect = [1..100] == map (`select` reverse [1..100])  [0..99]
