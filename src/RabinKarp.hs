ed6fbca3f758cc0815987b397aed073cddbd61b9
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-15 12:28:12 +0100

    suite rabin karp.

--- src/RabinKarp.hs
+++ src/RabinKarp.hs
@@ -22,15 +22,32 @@ rabinKarp p s = lookup (hash $ asciis p) . drop (fromMaybe 0 s) .
 
 -- Solution 2
 
+rabinKarp2 :: String -> String -> Int
+rabinKarp2 p text =
+        if p == ""
+                then -1
+        else
+                fromMaybe (-1) $ mapOnMaybe fst $ find matchingString $ zip [0..] $ scanl nextHash (hash2 text m) $ windowed (m+1) text
+        where n = length text
+              m = length p
+              nextHash currentHash chars = reHash currentHash (head chars) (last chars) m
+              matchingString (offset, textHash) = hash2 p m == textHash && p == subString text offset m
+
+mapOnMaybe :: (a -> b) -> Maybe a -> Maybe b
+mapOnMaybe fn (Just x) = Just (fn x)
+mapOnMaybe _ (Nothing) = Nothing
+
+subString text start end = take end $ drop start text
+
+windowed :: Int -> [a] -> [[a]]
+windowed size [] = []
+windowed size ls@(x:xs) = if length ls >= size then take size ls : windowed size xs else windowed size xs
+
 globalQ = 1920475943
 globalR = 256
 
--- ((r^m-1 * ascii char) + (r^m-2 * ascii char) + … (r0 * ascii char)) % q
--- where r = 256, q = 1920475943
--- so for hash "markusaerelius" 3 it is:
--- ((256^2 * ord ‘m’) + (2561 * ord ‘a’) + (2560 * ord ‘r’)) `mod` 1920475943
-hash' = hash'' globalR globalQ
-hash'' r q string m = foldl (\acc x -> (r * acc + ord x) `mod` q) 0 $ take m string
+hash2 = hash' globalR globalQ
+hash' r q string m = foldl (\acc x -> (r * acc + ord x) `mod` q) 0 $ take m string
 
 reHash = reHash' globalR globalQ
 reHash' r q existingHash firstChar nextChar m =
@@ -39,33 +56,14 @@ reHash' r q existingHash firstChar nextChar m =
                 rm = if m >0 then (fromIntegral r ^ fromIntegral (m-1)) `mod` fromIntegral q else 0
                 takeOffFirstChar = existingHash - fromIntegral rm * ord firstChar
 
-windowed :: Int -> String -> [String]
-windowed = undefined
 
--- subString :: String -> Int ->  Int -> Maybe Int 
--- subString = _
-
-rabinKarp2 :: String -> String -> Int
-rabinKarp2 text p =
-        if p == "" then -1
-        else fromMaybe (-1) $ mapToMaybe fst $ find matchingString $ zip [0..] $ scanl nextHash (hash' text m) $ windowed (m+1) text
-        where n = length text
-              m = length p
-              patternHash = hash' p m
-              nextHash currentHash chars = reHash currentHash (head chars) (last chars) m
-              matchingString (offset, textHash) = patternHash == textHash && p == subString text offset m
-
+assertEqual a b = print (a == b, a, b)
 
 test :: (String -> Maybe Int -> String -> Maybe Int) -> IO ()
 test f = do assertEqual (f ""   Nothing  "Hello World") (Just 0)
             assertEqual (f "He" Nothing  "Hello World") (Just 0)
-            assertEqual (f "od" Nothing  "Bonsai Code") (Just 8)
-            assertEqual (f "ef" Nothing  "Bonsai Code") Nothing
             assertEqual (f ""   (Just 1) "Hello World") (Just 1)
             assertEqual (f "He" (Just 1) "Hello World") Nothing
-            assertEqual (f "od" (Just 1) "Bonsai Code") (Just 8)
-            assertEqual (f "ef" (Just 1) "Bonsai Code") Nothing
-         where assertEqual a b = print (a == b, a, b)
 
 main :: IO ()
 main = test rabinKarp
