508ed9a69009cda973e1c9e1623093a1dac5fc86
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-25 11:44:35 +0100

    Adding memoization.

--- src/longestCommonSubsequence.hs
+++ src/longestCommonSubsequence.hs
@@ -1,5 +1,7 @@
 module LongestCommonSubsequence where
 
+import qualified Data.MemoCombinators as M
+
 longest xs ys = if length xs > length ys then xs else ys
 
 lcs [] _ = []
@@ -8,4 +10,10 @@ lcs (x:xs) (y:ys)
   | x == y = x : lcs xs ys
   | otherwise = longest (lcs (x:xs) ys) (lcs xs (y:ys))
 
+
+-- Memoized version
+memoize = M.memo2 mString mString
+mString = M.list M.char
+
+lcsm = memoize lcs
 
