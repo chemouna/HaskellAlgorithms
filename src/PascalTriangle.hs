ec47e6f23854c498b87c221e1b63c5e196b495b5
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-02 09:43:05 +0200

    3rd way to solve pascal

--- src/PascalTriangle.hs
+++ src/PascalTriangle.hs
@@ -7,3 +7,5 @@ pascal = iterate (\prev -> 1 : zipWith (+) prev (tail prev) ++ [1]) [1]
 
 next row = zipWith (+) (row ++ [0]) ([0] ++ row)
 pascal2 = iterate next [1]
+
+pascal3 = [1] : map (zipWith (+) <$> ([0] ++) <*> (++ [0])) pascal3
