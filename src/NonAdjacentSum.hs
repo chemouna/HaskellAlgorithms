4f77c3d1967980e540088f9a08d3e8ce83a76dd2
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-03 09:33:25 +0200

    Iterative solution for non adjacent sum.

--- src/NonAdjacentSum.hs
+++ src/NonAdjacentSum.hs
@@ -2,8 +2,10 @@
 module NonAdjacentSum where
 
 -- recursive repetitve solution (slow as it does a lot of calculations mutliple times)
-maxsum :: Num a => [a] -> a
 maxsum [] = 0
 maxsum [x] = x
-maxsum (x:y:xs) = (x + maxsum xs) + maxsum (y:xs)
+maxsum (x:y:xs) = max (x + maxsum xs) (maxsum (y:xs))
 
+-- iterative solution using foldl
+maxsum_iter xs = max a b where
+  (a, b) = foldl (\(i, e) x -> (x + e, max i e)) (0, 0) xs
