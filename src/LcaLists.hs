236c1528f2b40ba78968d8428bfd83fcad631125
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-03 23:57:13 +0200

    Lca lists.

--- src/LcaLists.hs
+++ src/LcaLists.hs
@@ -1,14 +1,28 @@
-
 module LcaLists where
+
+import Data.List
+import Test.QuickCheck
+import Test.QuickCheck.All
 
 lca :: Eq a => [a] -> [a] -> Maybe a
 lca xs ys = let (m, n) = (length xs, length ys) in
-  if m < n then match xs (drop (n - m) ys)
-  else match (drop (n - m) xs) ys
+  if m < n then match xs $ drop (n - m) ys
+  else match (drop (m - n) xs) ys
 
 match :: Eq a => [a] -> [a] -> Maybe a
 match [] _ = Nothing
 match _ [] = Nothing
-match (x:xs) (y:ys) 
+match (x:xs) (y:ys)
   | x == y = Just x
   | otherwise = match xs ys
+
+prop_lca :: [Int] -> Int -> Int -> Bool
+prop_lca xs n m = lca ys zs == if suffix == [] then Nothing else Just (head suffix)
+    where
+      xs' = nub (0:xs)
+      n' = (abs n) `mod` (length xs')
+      (asbs, suffix) = splitAt (1 + n') xs'
+      m' = (abs m) `mod` (length asbs)
+      (as, bs) = splitAt m' asbs
+      (ys, zs) = (as ++ suffix, bs ++ suffix)
+
