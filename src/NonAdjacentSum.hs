2ac7fbfb67a46722b1d142872d47231dda2fb110
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-03 09:43:18 +0200

    Implementation of infinite recursive solution.

--- src/NonAdjacentSum.hs
+++ src/NonAdjacentSum.hs
@@ -9,3 +9,9 @@ maxsum (x:y:xs) = max (x + maxsum xs) (maxsum (y:xs))
 -- iterative solution using foldl
 maxsum_iter xs = max a b where
   (a, b) = foldl (\(i, e) x -> (x + e, max i e)) (0, 0) xs
+
+-- lazy infinite recursive solution
+maxsums_lazy [] = []
+maxsums_lazy [x] = [0, x]
+maxsums_lazy (x:xs) = ms where
+  ms = 0 : x : zipWith3 (\x' m m' -> max (x' + m) m') xs ms (tail ms)
