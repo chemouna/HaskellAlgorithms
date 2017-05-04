40789671cd8ba22d91e09e521db61e904ae4b6a0
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-04 09:15:10 +0200

    Link tree nodes

---
+++ src/LinkTreeNodes.hs
@@ -0,0 +1,20 @@
+
+module LinkTreeNodes where
+
+data Tree a =  Empty | Branch (Tree a) a (Tree a)
+                deriving (Eq, Show)
+
+left (Branch l _ _) = l
+right (Branch _ _ r) = r
+root (Branch _ n _) = n
+
+link :: Eq a => Tree a -> [[a]]
+link t = link2 [[t]] [[root t]] where
+      link2 [] acc = reverse acc
+      link2 (ts:tss) acc = let ts2 = concat [filter (/= Empty) [left tr, right tr]
+                                            | tr <- ts] in
+          if null ts2 then link2 tss acc
+          else link2 (tss ++ [ts2]) (map root ts2:acc)
+
+
+
