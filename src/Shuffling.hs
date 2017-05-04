e37358db9fc76588e654b9efb978d98c64fec82d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-04 13:48:09 +0200

    Shuffling with the state monad.

--- src/Shuffling.hs
+++ src/Shuffling.hs
@@ -4,6 +4,10 @@ module Shuffling where
 import System.Random
 import Data.Array.IO
 import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Random
+import Data.Array.ST
+import GHC.Arr
 
 shuffle :: [a] -> IO [a]
 shuffle xs = do
@@ -18,3 +22,19 @@ shuffle xs = do
     n = length xs
     newArray :: Int -> [a] -> IO (IOArray Int a)
     newArray n = newListArray (1, n)
+
+
+shuffle' :: RandomGen g => [a] -> Rand g [a]
+shuffle' xs = do
+  let n = length xs
+  rands <- take n `fmap` getRandomRs (0, n-1)
+  let ar = runSTArray $ do
+      ar <- thawSTArray $ listArray (0, n-1) xs
+      forM_ (zip [0..(n-1)] rands) $ \(i, j) -> do
+         vi <- readSTArray ar i
+         vj <- readSTArray ar j
+         writeSTArray ar j vi
+         writeSTArray ar i vj
+      return ar
+  return (elems ar)
+
