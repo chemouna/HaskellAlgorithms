1f348c0cfa37294e73d45d36657ee20ff6f28ec1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-02 07:21:28 +0100

    Solution 3.

--- src/NextNumber.hs
+++ src/NextNumber.hs
@@ -7,6 +7,7 @@ import Data.Foldable
 import Control.Applicative
 import System.IO
 import Text.Printf
+import Data.Maybe
 
 {--
 main = jam $ do
@@ -18,7 +19,6 @@ main = jam $ do
           Just (xs, y:ys) -> xs ++ b:as ++ y:bs
             where (as, b:bs) = span (<= y) $ reverse ys
 --}
-
 
 
 main :: IO ()
@@ -40,3 +40,28 @@ f :: Char -> String -> String
 f a t = let nextLargest = head (sort (filter (>a) t))
             removed = sort (a:delete nextLargest t)
         in nextLargest:removed
+
+-- Solution 3
+nice :: [Char] -> Bool
+nice (a:as) = any (> a) as
+nice [] = False
+
+rearrange :: [Char] -> Maybe [Char]
+rearrange (a:as)
+        | Just ans <- rearrange as
+                = Just (a:ans)
+        | nice (a:as)
+                = let b = minimum [b | b <- as, b > a] in
+                        Just (b:sort (a: (as \\ [b])))
+rearrange _ = Nothing
+
+backup :: [Char] -> [Char]
+backup xs
+        | ys <- sort xs, y0 <- minimum [y | y <- ys, y /= '0']
+                = y0:'0':(ys \\ [y0])
+
+main' = do
+        n <- liftM read getLine
+        mapM_ (\ i -> do
+                xs <- getLine
+                putStrLn ("Case #" ++ show i ++ ": " ++ (fromMaybe (backup xs) (rearrange xs)))) [1..n]
