d5bb2ea4fdedf4f535256a80bb8f5c1182731bcc
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-11 08:07:15 +0000

    Added findMaxInList.

--- src/codeforces/Cf6BPresidentOffice.hs
+++ src/codeforces/Cf6BPresidentOffice.hs
@@ -1,8 +1,16 @@
-
 module Cf6BPresidentOffice where
 
 import Data.List
 import Control.Monad
+import Data.Ord
+
+{--
+i need to count adjacencies left and right of his color
+so first i need to locate the president color
+
+shld be instead adjacencies of all matches of its color and take max
+
+-}
 
 solve :: String -> [String] -> ([Integer], String)
 solve s m = (vs, c)
@@ -11,13 +19,18 @@ solve s m = (vs, c)
     c = last l
     l = words s
 
+-- ["G.B.",".RR.","TTT."]
+-- lets for now get just the furthest index and if that doest work get all and find max
+
+findMaxIndexList :: String -> Char -> Int
+findMaxIndexList s v = snd $ last $ filter (\(x,y) -> x == v) (zip s [0..])
 
 main :: IO ()
 main = do
   input <- getLine
   inputs <- replicateM 3 getLine
   print $ solve input inputs
- 
+
   -- solve
 
   -- s <- getLine
@@ -25,5 +38,3 @@ main = do
   --vs <- map (\x -> read x :: Integer) (init l)
   -- c <- last l
   -- putStrLn $  "test" -- l
-
-
