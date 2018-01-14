411da05811dce6c08644c73be7c66fcb19e554bf
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-14 18:17:54 +0000

    Solution to Fence problem in haskell.

---
+++ src/codeforces/363/Fence.hs
@@ -0,0 +1,16 @@
+module Fence where
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Arrow ((&&&))
+
+main :: IO ()
+main = print =<< solve <$> ((!!1) . map read . words <$> getLine) <*> (map read . words <$> getLine)
+
+solve :: Int -> [Int] -> Int
+solve k xs = snd . minimum $ flip zip [1..] sumsOfEachKElements
+  where
+     sumsOfEachKElements = uncurry (zipWith (-)) pairWithAndWithoutK
+     pairWithAndWithoutK = (drop k &&& id) progressivSums
+     progressivSums = scanl (+) 0 xs
+
+
