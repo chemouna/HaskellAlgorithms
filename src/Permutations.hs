08089a68b8d41280569349e5fc85e995930aea1d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-14 12:23:33 +0000

    Adding derangements.

--- src/Permutations.hs
+++ src/Permutations.hs
@@ -12,9 +12,21 @@ isPermutation xs (y:ys)
   | length xs /= length (y:ys) = False
   | otherwise = isPermutation (delete y xs) ys
 
-prop_perm :: Ord a => [a] -> Bool
-prop_perm ls = isPermutation ls (sort ls) == True
+perms :: Eq a => [a] -> [[a]]
+perms [] = [[]]
+perms xs = [ i:j | i <- xs, j <- perms $ delete i xs]
 
+isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement xs ys = and [x `elem` ys && index x xs /= index x ys | x <- xs]
+  where
+    index v (x:xs) | v == x = 0
+                   | otherwise = 1 + index v xs
+
+derangements :: Eq a => [a] -> [[a]]
+derangements xs = filter (\p -> isDerangement p xs) (perms xs)
+
+prop_isPerm :: Ord a => [a] -> Bool
+prop_isPerm ls = isPermutation ls (sort ls) == True
 
 return []
 main = ($verboseCheckAll)
