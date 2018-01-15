6f863de57e42c6675fc663d7dd6f63c93ee035e1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-15 22:35:51 +0000

    Add bytestring library.

---
+++ src/codeforces/451/SortTheArray.hs
@@ -0,0 +1,14 @@
+module SortTheArray where
+
+import Data.List
+import Text.Printf
+
+
+main :: IO ()
+main = do
+  _ <- getLine
+  s <- getLine
+  let t = getInts s
+      y = get [] t
+      u = got [] snd y 
+  putStrLn $ "Test"
